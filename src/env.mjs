545aaeec393c95d0eac7104065a1d08010f0546f
Abe Caymo <caymo.abesuni@gmail.com>
2023-04-27 13:19:24 +0800

    remove unnecessary env

--- src/env.mjs
+++ src/env.mjs
@@ -15,9 +15,11 @@ const server = z.object({
  * built with invalid env vars. To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 const client = z.object(
-  /** @satisfies {Record<`NEXT_PUBLIC_${string}`, import('zod').ZodType>} */ ({
-    NEXT_PUBLIC_SEGMENT_ID: z.string(),
-  })
+  /** @satisfies {Record<`NEXT_PUBLIC_${string}`, import('zod').ZodType>} */ (
+    {
+      // NEXT_PUBLIC_SEGMENT_ID: z.string(),
+    }
+  )
 );
 
 /**
@@ -30,7 +32,6 @@ const processEnv = {
   NODE_ENV: process.env.NODE_ENV,
   MONGODB_URL: process.env.MONGODB_URL,
   ACCOUNT_ID: process.env.ACCOUNT_ID,
-  NEXT_PUBLIC_SEGMENT_ID: process.env.NEXT_PUBLIC_SEGMENT_ID,
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
